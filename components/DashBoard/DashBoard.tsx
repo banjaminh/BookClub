b07ecc7374a9e1ae531b799aabbb9cc31e8c3d43
Judy Ye <judyy.ye@gmail.com>
2023-11-29 17:44:11 -0800

    Add TabNavigation

--- components/DashBoard/DashBoard.tsx
+++ components/DashBoard/DashBoard.tsx
@@ -1,10 +1,36 @@
 import { Text, View, SafeAreaView } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+const Tab = createBottomTabNavigator()
 
 export default function DashBoard(){
-    return (
-        <SafeAreaView>
-            <Text>DashBoard!</Text>
-        </SafeAreaView>
-    )
+
+  const dashboard = (
+    <Tab.Navigator
+      initialRouteName={'home'}
+    >
+      <Tab.Screen
+        name='home'
+        options={{ headerShown: false }}
+      />
+
+      <Tab.Screen
+        name='current'
+        options={{ headerShown: false }}
+      />
+
+      <Tab.Screen
+        name='search'
+        options={{ headerShown: false }}
+      />
+
+      <Tab.Screen
+        name='bookclub'
+        options={{ headerShown: false }}
+      />
+    </Tab.Navigator>
+  );
+
+  return dashboard;
 }
