3c5aeefde9cb59f026622912ffa9e010b29ac3a4
Alex Peterson <aspeterson.126@gmail.com>
2023-12-05 13:41:14 -0600

    Refactor Header component in App

--- App.tsx
+++ App.tsx
@@ -1,34 +1,31 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import DashBoard from "./components/DashBoard/DashBoard";
-import Header from "./components/Header/Header";
-
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import DashBoard from './components/DashBoard/DashBoard'
+import Header from './components/Header/Header'
 
 export default function App() {
-	const Stack = createStackNavigator();
+  const Stack = createStackNavigator()
 
-	return (
-		<NavigationContainer>
-			<Stack.Navigator
-				screenOptions={{
-                    header: ({ scene, previous, navigation }) => {
-                        return <Header />;
-                    },
-                }}
-			>
-				<Stack.Screen name="DashBoard" component={DashBoard} />
-			</Stack.Navigator>
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          header: () => <Header />
+        }}
+      >
+        <Stack.Screen name='DashBoard' component={DashBoard} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
