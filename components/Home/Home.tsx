d3c392568e7ec6966b885a3ed1723b32edcd4e67
Alex Peterson <aspeterson.126@gmail.com>
2023-12-06 14:13:28 -0600

    Add api call

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,7 +1,23 @@
 import { View, Text } from "react-native";
+import { useEffect } from "react";
 import { styles } from "./HomeStyleSheet";
+// import { gatherLatest } from "../../apiCalls"
 
 export default function Home() {
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const info = await gatherLatest()
+  //       console.log(info)
+  //     } catch(error) {
+  //       console.log(error)
+  //     }
+  //   } 
+
+  //   fetchData()
+  // }, [])
+
   return(
     <View style={styles.homeContainer}>
       <Text>Home</Text>
