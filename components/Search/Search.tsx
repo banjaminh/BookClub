1d20bda1f9ce2e0b771f399403faed2fccb01764
Banjamin Hatch <banjaminhatch@gmail.com>
2023-12-13 12:44:38 -0700

    Adds function for searching the booktypes

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,10 +1,45 @@
-import { View, Text } from "react-native";
+import {
+	View,
+	Text,
+	TouchableWithoutFeedback,
+	Keyboard,
+	TextInput,
+} from "react-native";
 import { styles } from "./SearchStyleSheet";
+import { useState } from "react";
+import { bookTypes } from "../../list_names";
 
 export default function Search() {
-  return (
-    <View style={styles.searchContainer}>
-      <Text>Search</Text>
-    </View>
-  )
+	const [input, setInput] = useState<string>("");
+	const [suggestions, setSuggestions] = useState<string[]>([]);
+
+  function searchBookTypes(searchInput: string){
+    const results = bookTypes.filter(type => { 
+      const splitArray = type.split(' ');
+      const isTrue = splitArray.some(split => split.toLowerCase().startsWith(searchInput.toLowerCase()));
+      return isTrue;
+      
+    })
+    return results
+  }
+
+	function createSuggestions(searchText: string) {
+
+  }
+  searchBookTypes("nonfiction")
+	function onChangeText(text: string) {
+		setInput(text);
+	}
+
+	return (
+		<View style={styles.searchContainer}>
+			<Text>Search</Text>
+			<TextInput
+				placeholder="Book Type"
+				value={input}
+				onChangeText={onChangeText}
+				style={styles.searchInput}
+			/>
+		</View>
+	);
 }
--- components/Search/SearchStyleSheet.tsx
+++ components/Search/SearchStyleSheet.tsx
@@ -5,5 +5,15 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
+
+  searchInput:{
+    height: 40,
+    width: 250,
+    marginHorizontal: 12,
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+  },
+
 })
