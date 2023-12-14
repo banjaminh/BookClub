3d4cc4520e7cc0946047ddc290b5216da3cf8279
Jocelyn Wensloff <jewensloff@gmail.com>
2023-12-14 15:09:56 -0700

    Display current books

--- components/Current/Current.tsx
+++ components/Current/Current.tsx
@@ -1,10 +1,42 @@
-import { View, Text } from "react-native";
-import { styles } from "./CurrentStyleSheet";
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native';
+import { styles } from './CurrentStyleSheet';
+import { bookData } from '../BookScrollView/testData';
+import { Book } from '../../types';
+import { useState } from 'react';
+import { allBookClubs } from '../../mockBookClubData';
 
 export default function Current() {
+  const [books, setBooks] = useState<Book[]>(bookData.results.books);
+  const [currentBooks, setCurrentBooks] = useState<Book[]>([
+    bookData.results.books[5],
+    bookData.results.books[6],
+  ]);
+
+  const [currentClub, setCurrentClub] = useState<string>('Reading Rabbits');
+const toggleModal = () => {
+  console.log('I was clicked ')
+}
+  const renderItem = ({ item }: { item: Book }) => (
+    <TouchableOpacity onPress = {() => toggleModal()}>
+      <Image
+        style={styles.bookImage}
+        source={{
+          uri: item.book_image,
+        }}
+        resizeMode='cover'
+      />
+    </TouchableOpacity>
+  );
+
   return (
     <View style={styles.currentContainer}>
-      <Text>Current</Text>
+      <Text>Club: {currentClub}</Text>
+      <Text>Currently Reading</Text>
+      <FlatList 
+        data={currentBooks} 
+        renderItem={renderItem} 
+        horizontal={true} 
+      />
     </View>
-  )
-} 
+  );
+}
--- components/Current/CurrentStyleSheet.tsx
+++ components/Current/CurrentStyleSheet.tsx
@@ -5,5 +5,18 @@ export const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  bookImage: {
+    width: 125,
+    height: 220,
+    // borderRadius: 10,
+    // shadowColor: 'black',
+    // shadowOffset: { width: 0, height: 2 },
+    // shadowOpacity: 1,
+    // shadowRadius: 4,
+    overflow: 'hidden',
+
+    alignSelf: 'center',
+    marginBottom: 5
   }
 })
