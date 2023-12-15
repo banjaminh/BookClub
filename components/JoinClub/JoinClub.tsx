d0f73d1880256b73d993be5592bd11d3f53a0833
Alex Peterson <aspeterson.126@gmail.com>
2023-12-15 11:30:44 -0600

    Create joinClub age w/ initial styling

---
+++ components/JoinClub/JoinClub.tsx
@@ -0,0 +1,22 @@
+import {
+	View,
+	Text,
+	TouchableOpacity,
+} from "react-native";
+import { styles } from "./JoinClubStyleSheet";
+import { useNavigation } from "@react-navigation/native";
+
+export default function JoinClub() {
+const navigation = useNavigation();
+
+  return (
+    <View style={styles.joinClubContainer}> 
+        <View style={styles.internalNavigation}>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('BookClub')}>
+            <Text> ⬅️ Return to Your Clubs</Text>
+            </TouchableOpacity>
+        </View>
+      <Text style={styles.heading}>Join a Club:</Text>
+    </View>
+  )
+} 
---
+++ components/JoinClub/JoinClubStyleSheet.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  joinClubContainer: {
+    flex: 1,
+    marginTop: 80,
+  },
+  internalNavigation: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingLeft: 10,
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  heading: {
+    marginTop: 40,
+    marginBottom: 20,
+    paddingLeft: 10,
+  },
+})
