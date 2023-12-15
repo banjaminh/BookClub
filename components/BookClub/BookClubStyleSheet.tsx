1c4a669419a1a654742c0c54c8fa85a832947f2a
Alex Peterson <aspeterson.126@gmail.com>
2023-12-15 11:31:02 -0600

    Add navigation to BookClub page w/ initial styling

--- components/BookClub/BookClubStyleSheet.tsx
+++ components/BookClub/BookClubStyleSheet.tsx
@@ -3,7 +3,43 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
   bookClubContainer: {
     flex: 1,
+    marginTop: 80,
+  },
+  internalNavigation: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingLeft: 10,
+  },
+  button: {
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 10,
+    backgroundColor: '#EAB464',
+    marginRight: 20,
+    borderRadius: 10,
+  },
+  heading: {
+    marginTop: 40,
+    marginBottom: 20,
+    paddingLeft: 10,
+  },
+  allClubs: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    paddingTop: 20,
+  },
+  club: {
+    alignItems: 'center',
+    borderColor: '#C84630',
+    borderWidth: 3,
+    borderRadius: 10,
+    backgroundColor: 'white',
+    width: 150,
+    height: 200,
+  },
+  clubHeading: {
+    marginTop: 10,
   }
 })
